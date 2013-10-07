bf553e71232a68feb6ac590db1495a77e32f5128
Compomics User <compomics@codeb.UGent.be>
2013-10-07 13:52:09 +0200

    output process

--- source/protein_digestion.R
+++ source/protein_digestion.R
@@ -1,6 +1,11 @@
 
 
-projectPath <- "C:/Users/Nicolas Housset/Documents/R_Projects/FocusHydrophil"
+# projectPath <- "C:/Users/Nicolas Housset/Documents/R_Projects/FocusHydrophil"
 
+projectPath <- "/mnt/compomics/Nicolas/R_Projects/FocusHydrophil"
 yeastPath <- "/data/Protein/Yeast"
 
+peptides <- data.table(read.table(file = paste0(projectPath, yeastPath, "/Yeast_OneMiss.txt"),
+                       sep = "", col.names=c("Type","Sequence","Probability"),
+                       fill = TRUE
+                       ))
